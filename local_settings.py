6438b3659fb9bff4e0c78097ba32e6df234250ca
Chow Loong Jin <hyperair@debian.org>
2015-09-03 07:14:10 +0800

    Set PIPELINE_YUI_BINARY in local_settings

--- local_settings.py
+++ local_settings.py
@@ -12,3 +12,5 @@ for key, destkey in openshift_keys:
 
 PIPELINE_SASS_BINARY = os.path.join(os.environ['OPENSHIFT_DATA_DIR'],
                                     'opt', 'sass', 'bin', 'sass')
+PIPELINE_YUI_BINARY = os.path.join(os.path.dirname(__file__),
+                                   'bin', 'yui-compressor')
